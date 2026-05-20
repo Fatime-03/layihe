ab5b806a32e308eaede11290f1d0465f6d0d8980
Fatime-03 <fatimeyusifli42@gmail.com>
2026-05-20 09:26:32 +0400

    Add Isci class with properties for employee details

---
+++ isci.cs
@@ -0,0 +1,8 @@
+public class Isci
+{
+    public int Id { get; set; }
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public string Vezife { get; set; }
+    public decimal Maas { get; set; }
+}
